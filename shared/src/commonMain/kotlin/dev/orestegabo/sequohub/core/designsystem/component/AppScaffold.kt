87c81b0444f5c934f65cf1492bc45835e3f9c003
oreste <muhirwa.g.oreste@gmail.com>
2026-09-12 03:02:16 +0200

    Add AppScroll design system component

---
+++ shared/src/commonMain/kotlin/dev/orestegabo/sequohub/core/designsystem/component/AppScaffold.kt
@@ -0,0 +1,31 @@
+package dev.orestegabo.sequohub.core.designsystem.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AppScroll(
+    content: @Composable ColumnScope.() -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .safeContentPadding()
+            .verticalScroll(rememberScrollState())
+            .padding(start = 0.dp, top = 18.dp, end = 0.dp, bottom = 112.dp),
+        verticalArrangement = Arrangement.spacedBy(14.dp),
+        content = content,
+    )
+}
