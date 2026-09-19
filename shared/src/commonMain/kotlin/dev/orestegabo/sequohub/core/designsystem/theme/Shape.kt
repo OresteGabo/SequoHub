a3855f4bd9641cbe0fc63a32f3a432f593ac3320
oreste <muhirwa.g.oreste@gmail.com>
2026-09-19 15:31:39 +0200

    Define `AppShapes` in the design system theme

---
+++ shared/src/commonMain/kotlin/dev/orestegabo/sequohub/core/designsystem/theme/Shape.kt
@@ -0,0 +1,13 @@
+package dev.orestegabo.sequohub.core.designsystem.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val AppShapes = Shapes(
+    extraSmall = RoundedCornerShape(6.dp),
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(8.dp),
+    large = RoundedCornerShape(12.dp),
+    extraLarge = RoundedCornerShape(18.dp),
+)
