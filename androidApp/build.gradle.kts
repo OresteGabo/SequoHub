dd85a911418711391230e68be1ca9756d1adb477
oreste <muhirwa.g.oreste@gmail.com>
2026-09-22 14:56:40 +0200

    Add Credentials and GoogleID dependencies to androidApp
    - Add `androidx.credentials`, `androidx.credentials.playServicesAuth`, and `googleid` to the project dependencies.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,6 +14,9 @@ dependencies {
     implementation(project(":shared"))
 
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.playServicesAuth)
+    implementation(libs.googleid)
 
     implementation(libs.compose.uiToolingPreview)
     debugImplementation(libs.compose.uiTooling)
